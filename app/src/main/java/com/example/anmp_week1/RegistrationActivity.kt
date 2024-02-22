59c13f78b2956866c784cdcc0eeadc7d642dc81f
Andre <andre@staff.ubaya.ac.id>
2024-02-22 17:24:05 +0700

    Registration Activity WIP - comments added

--- app/src/main/java/com/example/anmp_week1/RegistrationActivity.kt
+++ app/src/main/java/com/example/anmp_week1/RegistrationActivity.kt
@@ -7,5 +7,9 @@ class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        // i have to check for existing email
+        // i have to check for password
+        // i have to implements OTP
     }
 }
